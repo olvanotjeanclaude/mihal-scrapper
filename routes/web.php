ca71887d0ae98bd7b979ef0ce067b1f82a1f6a94
olvanotjeanclaude <olvanotpoint@gmail.com>
2024-03-07 01:39:11 +0300

    adding simone questions

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/",[\App\Http\Controllers\SimoneController::class,"questions"]);
+Route::get("/questions",[\App\Http\Controllers\SimoneController::class,"questions"]);
 Route::get("/questions/{id}",[\App\Http\Controllers\SimoneController::class,"question"]);
